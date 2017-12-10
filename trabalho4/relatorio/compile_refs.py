9a2a25e07c4f05e92b4550fcfcca3086c7b1b52a
afonscosta <afonso.costa.15@gmail.com>
2017-12-10 22:25:23 +0000

    Realizadas as perguntas 1,2,3

---
+++ trabalho4/relatorio/compile_refs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#Compilar: python compile_refs.py lncs-paper
+
+import subprocess, sys
+
+commands = [
+    ['pdflatex', sys.argv[1] + '.tex'],
+     ['bibtex', sys.argv[1] + '.aux'],
+     ['pdflatex', sys.argv[1] + '.tex'],
+     ['pdflatex', sys.argv[1] + '.tex']
+]
+
+for c in commands:
+    subprocess.call(c)
